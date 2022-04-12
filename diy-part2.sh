23712adfd378c3905828b784197f00c9cd0012f6
XQY3Ker <58332287+XQY3Ker@users.noreply.github.com>
2022-04-12 17:37:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,6 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+sed -i 's/\${vendorid:+-V "$vendorid"}/-V \"\"/g' package/network/config/netifd/files/lib/netifd/proto/dhcp.sh
